b7b736282d95fba00d89c5a0f1c2156b557dcd96
tkamit6 <76872254+tkamit6@users.noreply.github.com>
2023-10-03 23:26:08 +0530

    sever

--- server.js
+++ server.js
@@ -1,11 +1,47 @@
+// const bodyParser = require('body-parser');
 const express = require('express');
-const server = express();
-const path = require('path');
+const mongoose = require('mongoose');
+cors = require('cors');
+bodyParser = require('body-parser');
+dbConfig = require('./src/Data-Base/db');
 
-server.use(express.static(path.join(__dirname, 'public')));
 
-server.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+// connet monggos
+mongoose.Promise = global.Promise;
+mongoose.connect(dbConfig.db,{
+    useUnifiedTopology: true, useNewUrlParser: true
+}).then(()=>{
+    console.log("Database Connected")
+},
+    error => {
+        console.log("Database not connected: " + error)
+    }
+)
+
+// setup express
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(cors());
+app.use(express.static(__dirname));
+
+const port = process.env.PORT || 4001;
+const server = app.listen(port,()=>{
+    console.log("Connected to port "+ port)
+})
+
+// Error
+app.use((req, res, next)=>{
+    // Error goes via next() method
+    setImmediate(()=>{
+        next(new Error("something went wrong"));
+    });
 });
 
-module.exports = server;
+app.use(function(err, req, res, next){
+    console.error(err.message);
+    if(!err.statusCode) err.statusCode = 500;
+    res.status(err.statusCode).send(err.message);
+});
