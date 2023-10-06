8831ff8eb598305b9d2536f8d83338be65a5cc70
tkamit6 <76872254+tkamit6@users.noreply.github.com>
2023-10-06 13:34:33 +0530

    p

--- frontend/src/pages/BusinessPage/Section7.jsx
+++ frontend/src/pages/BusinessPage/Section7.jsx
@@ -66,7 +66,7 @@ const Section7 = () => {
         console.log(formData);
         try {
             setloading(true)
-            const res = await axios.post('https://kamao-api.vercel.app/sendmail/business', formData).then(res => console.log(res));
+            const resp = await axios.post('https://kamao-api.vercel.app/sendmail/business', formData).then(res => console.log(res));
             toast.success('Thanks for sharing your details. We will contact you shortly.')
             setloading(false)
             setStep(1)
